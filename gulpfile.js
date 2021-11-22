028424367eb162f05c26cf8cf8b55a7546385e0f
神楽坂雅詩 <cyscxchope@gmail.com>
2021-11-22 19:14:53 +0800

    开始创建新版本(H5)

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+// 引入压缩需要的插件
+const gulp = require('gulp');
+const uglify = require('gulp-uglify'); // 压缩js
+const css = require('gulp-clean-css'); // 压缩css
+const html = require('gulp-htmlmin'); // 压缩html
+
+// 压缩js
+gulp.task('js', function () {
+    return gulp.src('./src/*.js') // 输入文件
+        .pipe(uglify()) // 执行压缩
+        .pipe(gulp.dest('dist/js')); // 输出文件
+});
+
+// 压缩css
+gulp.task('css', async () => {
+    await gulp.src(['./src/*.css']) // 输入文件
+        .pipe(css()) // 执行压缩
+        .pipe(gulp.dest('./dist/')); // 输出文件
+});
+
+// 压缩html
+gulp.task('html', async () => {
+    await gulp.src(['./src/*.html','./templates/*.html']) // 输入文件
+        .pipe(html()) // 执行压缩
+        .pipe(gulp.dest('./dist/')); // 输出文件
+});
